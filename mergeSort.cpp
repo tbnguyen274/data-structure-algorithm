5d507b91ef3a427fdf7340afa05b8c4f533154af
Bao Nguyen <124044476+tbnguyen274@users.noreply.github.com>
2024-07-16 22:38:15 +0700

    implement heapSort

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -55,7 +55,9 @@ void merge(int*& A, int p, int q, int r) {
     delete[] R;
 }
 
-void mergeSort(int*& A, int p, int r) {
+// merge sort
+// Time complexity: O(nlogn)
+void mergeSort(int* A, int p, int r) {
     if (p >= r) 
         return;
     int q = (p + r) / 2;
@@ -65,7 +67,8 @@ void mergeSort(int*& A, int p, int r) {
 }
 
 // insertion sort
-void insertionSort(int*& a, int n) {
+// Time complexity: O(n^2)
+void insertionSort(int* a, int n) {
     for (int i = 1; i < n; i++) {
         int key = a[i];
         int j = i - 1;
@@ -78,7 +81,8 @@ void insertionSort(int*& a, int n) {
 }
 
 // bubble sort
-void bubbleSort(int*& a, int n) {
+// Time complexity: O(n^2)
+void bubbleSort(int* a, int n) {
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n-i-1; j++) {
             if (a[j] > a[j+1]) {
@@ -87,19 +91,82 @@ void bubbleSort(int*& a, int n) {
                 a[j+1] = temp;
             }
         }
-        print(a, n);
     }
+}
 
+// heap sort
+// array representing heap starts at index 1, index 0 is ignored
+// Time complexity: O(nlogn)
+int leftChildIndex(int a[], int n, int i) {
+    if (2*i <= n) {
+        return 2*i;
+    }
+    return -1;
+}
+
+int rightChildIndex(int a[], int n, int i) {
+    if (2*i+1 <= n) {
+        return 2*i+1;
+    }
+    return -1;
+}
+
+int parentIndex(int a[], int n, int i) {
+    if (i > 1 && i <= n) {
+        return i/2;
+    }
+    return -1;
+}
+
+// max-heapify
+// Time complexity: O(logn)
+void maxHeapify(int a[], int n, int i) {
+    int l = leftChildIndex(a, n, i);
+    int r = rightChildIndex(a, n, i);
+    int largest = i;
+    if (l != -1 && a[l] > a[largest]) {
+        largest = l;
+    }
+    if (r != -1 && a[r] > a[largest]) {
+        largest = r;
+    }
+    if (largest != i) {
+        int temp = a[i];
+        a[i] = a[largest];
+        a[largest] = temp;
+        maxHeapify(a, n, largest);
+    }
+}
+
+// build max heap
+// Time complexity: O(n)
+void buildMaxHeap(int a[], int n) {
+    for (int i = n/2; i >= 1; i--) {
+        maxHeapify(a, n, i);
+    }
+}
+
+// heap sort
+// Time complexity: O(nlogn)
+void heapSort(int a[], int n) {
+    buildMaxHeap(a, n);
+    for (int i = n; i >= 2; i--) {
+        int temp = a[1];
+        a[1] = a[i];
+        a[i] = temp;
+        maxHeapify(a, i-1, 1);
+    }
 }
 
 int main() {
-    int* A = new int[8] {2, 4, 5, 7, 1, 8, 3, 6};
     int n = 8;
+    int* A = new int[n+1] {0, 2, 4, 5, 7, 1, 8, 3, 6};
     print(A, n);
     //mergeSort(A, 0, n-1);
     //insertionSort(A, n);
-    bubbleSort(A, n);
-    print(A, n);
+    //bubbleSort(A, n);
+    heapSort(A, n);
+    print(A, n+1);
 
     return 0;
 }
