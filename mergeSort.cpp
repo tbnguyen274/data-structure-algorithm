b273c481941e51087ffa465edf41a267233854c3
Bao Nguyen <124044476+tbnguyen274@users.noreply.github.com>
2024-07-16 17:51:06 +0700

    insertion and bubble sort

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -64,11 +64,41 @@ void mergeSort(int*& A, int p, int r) {
     merge(A, p, q, r);
 }
 
+// insertion sort
+void insertionSort(int*& a, int n) {
+    for (int i = 1; i < n; i++) {
+        int key = a[i];
+        int j = i - 1;
+        while (j >= 0 && key < a[j]) {
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = key;
+    }
+}
+
+// bubble sort
+void bubbleSort(int*& a, int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (a[j] > a[j+1]) {
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+        print(a, n);
+    }
+
+}
+
 int main() {
-    int* A = new int[8] {2, 4, 5, 7, 1, 0, 3, 6};
+    int* A = new int[8] {2, 4, 5, 7, 1, 8, 3, 6};
     int n = 8;
     print(A, n);
-    mergeSort(A, 0, n-1);
+    //mergeSort(A, 0, n-1);
+    //insertionSort(A, n);
+    bubbleSort(A, n);
     print(A, n);
 
     return 0;
