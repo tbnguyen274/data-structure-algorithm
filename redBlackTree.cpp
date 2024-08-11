70dfdc5ae26123ed86a7cc2e4e253ec190997011
Bao Nguyen <124044476+tbnguyen274@users.noreply.github.com>
2024-08-11 14:18:46 +0700

    implementations of insert and delete RB trees, intro to AVL trees

--- redBlackTree.cpp
+++ redBlackTree.cpp
@@ -12,6 +12,7 @@ properties:
 5. For each node, all simple paths from the node to descendant leaves contain the same
 number of black nodes.
 */
+
 #include <iostream>
 using namespace std;
 
@@ -107,6 +108,8 @@ void insertFixup(TreeNode* root, TreeNode* node) {
     root->color = 0;
 }
 
+// Time complexity: O(log n)
+// Never perform more than 2 rotations
 void insertRB (TreeNode* root, TreeNode* node) {
     TreeNode* x = root;
     TreeNode* y = NULL;
@@ -131,3 +134,126 @@ void insertRB (TreeNode* root, TreeNode* node) {
     node->color = 1;
     insertFixup(root, node);
 }
+
+void transplantRB(TreeNode* root, TreeNode* u, TreeNode* v) {
+    if (u->parent == NULL) {
+        root = v;
+    } else if (u == u->parent->left) {
+        u->parent->left = v;
+    } else {
+        u->parent->right = v;
+    }
+    v->parent = u->parent;
+}
+
+TreeNode* minimumSearch(TreeNode* root) {
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+void deleteFixup(TreeNode* root, TreeNode* node) {
+    while (node != root && node->color == 0) {
+        if (node == node->parent->left) {
+            TreeNode* w = node->parent->right;
+
+            // case 1
+            if (w->color == 1) {
+                w->color = 0;
+                node->parent->color = 1;
+                leftRotate(root, node->parent);
+                w = node->parent->right;
+            }
+
+            // case 2
+            if (w->left->color == 0 && w->right->color == 0) {
+                w->color = 1;
+                node = node->parent;
+            }
+            else {
+                // case 3
+                if (w->right->color == 0) {
+                    w->left->color = 0;
+                    w->color = 1;
+                    rightRotate(root, w);
+                    w = node->parent->right;
+                }
+
+                // case 4
+                w->color = node->parent->color;
+                node->parent->color = 0;
+                w->right->color = 0;
+                leftRotate(root, node->parent);
+                node = root;
+            }
+        }
+        // "right" and "left" exchanged
+        else {
+            TreeNode* w = node->parent->left;
+            if (w->color == 1) {
+                w->color = 0;
+                node->parent->color = 1;
+                rightRotate(root, node->parent);
+                w = node->parent->left;
+            }
+            if (w->right->color == 0 && w->left->color == 0) {
+                w->color = 1;
+                node = node->parent;
+            } else {
+                if (w->left->color == 0) {
+                    w->right->color = 0;
+                    w->color = 1;
+                    leftRotate(root, w);
+                    w = node->parent->left;
+                }
+                w->color = node->parent->color;
+                node->parent->color = 0;
+                w->left->color = 0;
+                rightRotate(root, node->parent);
+                node = root;
+            }
+        }
+    }
+    node->color = 0;
+}
+
+// Time complexity: O(log n)
+void deleteRB(TreeNode* root, TreeNode* node) {
+    TreeNode* y = node;
+    TreeNode* x;
+    bool yOriginalColor = y->color;
+    if (node->left == NULL) {
+        x = node->right;
+        transplantRB(root, node, node->right);
+    } else if (node->right == NULL) {
+        x = node->left;
+        transplantRB(root, node, node->left);
+    } else {
+        y = minimumSearch(node->right);
+        yOriginalColor = y->color;
+        x = y->right;
+        if (y->parent == node) {
+            x->parent = y;
+        } else {
+            transplantRB(root, y, y->right);
+            y->right = node->right;
+            y->right->parent = y;
+        }
+        transplantRB(root, node, y);
+        y->left = node->left;
+        y->left->parent = y;
+        y->color = node->color;
+    }
+    if (yOriginalColor == 0) {
+        deleteFixup(root, x);
+    }
+}
+
+/*
+------------ AVL tree ----------------
+An AVL tree is a binary search tree that is height balanced: for each node x, the heights of the left and right subtrees of x differ by at most 1. To implement an AVL tree, maintain an extra attribute h in each node such that x:h is the height of node x. As for any other binary search tree T , assume that T:root points to the root 
+node.
+
+The height of an AVL tree storing n keys is O(log n). The AVL tree is named after its inventors, Adelson-Velskii and Landis.
+*/
